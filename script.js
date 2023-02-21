72e6c8f6ea8763e3d051b15c2d585c069ff789b7
Soluce7 <125985411+Soluce7@users.noreply.github.com>
2023-02-21 20:00:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 function rollDice() {
   var dice1 = document.getElementById("dice1");
   var dice2 = document.getElementById("dice2");
-  var dice3 = document.getElementById("dice3");
-
-  var dice1Value = Math.floor(Math.random() * 6) + 1;
-  var dice2Value = Math.floor(Math.random() * 6) + 1;
-  var dice3Value = Math.floor(Math.random() * 6) + 1;
-
-  dice1.innerHTML = dice1Value;
-  dice2.innerHTML = dice2Value;
-  dice3.innerHTML = dice3Value;
-}
+  var dice3 = document.getElementById("
