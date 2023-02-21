ac19ab77558eceec73d2bfcdccf79ff5c4cd4638
Soluce7 <125985411+Soluce7@users.noreply.github.com>
2023-02-21 20:07:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
+const dice = document.querySelectorAll(".die-list");
+const diceArray = [...dice];
+const button = document.getElementById("roll-button");
+
 function rollDice() {
-  var dice1 = document.getElementById("dice1");
-  var dice2 = document.getElementById("dice2");
-  var dice3 = document.getElementById("
+  button.disabled = true;
+  diceArray.forEach((die) => {
+    toggleClasses(die);
+    const randomNumber = getRandomNumber(1, 6);
+    updateDice(die, randomNumber);
+  });
+
+  setTimeout(() => {
+    button.disabled = false;
+  }, 1000);
+}
+
+function toggleClasses(die) {
+  die.classList.toggle("odd-roll");
+  die.classList.toggle("even-roll");
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function updateDice(die, randomNumber) {
+  die.dataset.roll = randomNumber;
+  switch (randomNumber) {
+    case 1:
+      die.innerHTML = '<span class="dot"></span>';
+      break;
+    case 2:
+      die.innerHTML = '<span class="dot"></span><span class="dot"></span>';
+      break;
+    case 3:
+      die.innerHTML =
+        '<span class="dot"></span><span class="dot"></span><span class="dot"></span>';
+      break;
+    case 4:
+      die.innerHTML =
+        '<span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>';
+      break;
+    case 5:
+      die.innerHTML =
+        '<span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>';
+      break;
+    case 6:
+      die.innerHTML =
+        '<span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>';
+      break;
+    default:
+      die.innerHTML = "X";
+  }
+}
+
+if (button) {
+  button.addEventListener("click", rollDice);
+}
