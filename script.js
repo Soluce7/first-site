ba7334c304c170c0c782c8f32b6067d715dde0fe
Soluce7 <125985411+Soluce7@users.noreply.github.com>
2023-02-22 00:34:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,14 @@
-const dice = document.querySelectorAll(".die-list");
-const diceArray = [...dice];
-const button = document.getElementById("roll-button");
-
 function rollDice() {
-  button.disabled = true;
-  diceArray.forEach((die) => {
-    toggleClasses(die);
-    const randomNumber = getRandomNumber(1, 6);
-    updateDice(die, randomNumber);
-  });
-
-  setTimeout(() => {
-    button.disabled = false;
-  }, 1000);
+  let dice1 = Math.floor(Math.random() * 6) + 1;
+  let dice2 = Math.floor(Math.random() * 6) + 1;
+  let dice3 = Math.floor(Math.random() * 6) + 1;
+  
+  let result1 = document.getElementById('result1');
+  let result2 = document.getElementById('result2');
+  let result3 = document.getElementById('result3');
+  
+  result1.innerHTML = dice1;
+  result2.innerHTML = dice2;
+  result3.innerHTML = dice3;
 }
 
-function toggleClasses(die) {
-  die.classList.toggle("odd-roll");
-  die.classList.toggle("even-roll");
-}
-
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function updateDice(die, randomNumber) {
-  die.dataset.roll = randomNumber;
-  switch (randomNumber) {
-    case 1:
-      die.innerHTML = '<span class="dot"></span>';
-      break;
-    case 2:
-      die.innerHTML = '<span class="dot"></span><span class="dot"></span>';
-      break;
-    case 3:
-      die.innerHTML =
-        '<span class="dot"></span><span class="dot"></span><span class="dot"></span>';
-      break;
-    case 4:
-      die.innerHTML =
-        '<span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>';
-      break;
-    case 5:
-      die.innerHTML =
-        '<span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>';
-      break;
-    case 6:
-      die.innerHTML =
-        '<span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>';
-      break;
-    default:
-      die.innerHTML = "X";
-  }
-}
-
-if (button) {
-  button.addEventListener("click", rollDice);
-}
