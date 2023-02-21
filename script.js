5975a482fe23e47f33a05c6dfb18504ed3649385
Soluce7 <125985411+Soluce7@users.noreply.github.com>
2023-02-21 17:36:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Select the navigation links and add a click event
+function rollDice() {
+  var dice1 = document.getElementById("dice1");
+  var dice2 = document.getElementById("dice2");
+  var dice3 = document.getElementById("dice3");
+  
+  var dice1Value = Math.floor(Math.random() * 6) + 1;
+  var dice2Value = Math.floor(Math.random() * 6) + 1;
+  var dice3Value = Math.floor(Math.random() * 6) + 1;
+  
+  dice1.src = "https://dummyimage.com/100x100/000/fff&text=" + dice1Value;
+  dice2.src = "https://dummyimage.com/100x100/000/fff&text=" + dice2Value;
+  dice3.src = "https://dummyimage.com/100x100/000/fff&text=" + dice3Value;
+}
