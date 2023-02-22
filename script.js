b9667f14a017cdd0a5216868cca9ba7bc46f9c10
Soluce7 <125985411+Soluce7@users.noreply.github.com>
2023-02-22 10:09:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ rollBtn.addEventListener("click", () => {
   });
 });
 
+function rollDice() {
+  // Get the dice squares
+  const dice1 = document.getElementById('dice1');
+  const dice2 = document.getElementById('dice2');
+  const dice3 = document.getElementById('dice3');
+  
+  // Generate random numbers for each dice
+  const value1 = Math.floor(Math.random() * 6) + 1;
+  const value2 = Math.floor(Math.random() * 6) + 1;
+  const value3 = Math.floor(Math.random() * 6) + 1;
+  
+  // Update the values of the dice squares
+  dice1.innerText = value1;
+  dice2.innerText = value2;
+  dice3.innerText = value3;
+}
