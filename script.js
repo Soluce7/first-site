8a87edee5c9bb44d0084f1516afdb5e178253145
Soluce7 <125985411+Soluce7@users.noreply.github.com>
2023-02-22 12:33:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 const dice1 = document.getElementById('dice1');
 const dice2 = document.getElementById('dice2');
 const dice3 = document.getElementById('dice3');
-const rollDiceBtn = document.querySelector('.roll-button');
-
-function rollDice() {
-  const diceValues = [];
-  for (let i = 0; i < 3; i++) {
-    const randomValue = Math.floor(Math.random() * 6) + 1;
-    diceValues.push(randomValue);
-  }
-  dice1.textContent = diceValues[0];
-  dice2.textContent = diceValues[1];
-  dice3.textContent = diceValues[2];
-}
-
-rollDiceBtn.addEventListener('click', rollDice);
+const rollDiceBtn = document.getElementById('roll-dice-btn');
+const standBtn = document.getElementById('stand-btn');
+const throwsCountEl = document.getElementById('throws-count');
+const scoreList = document.getElementById('score-list');
+let throwsCount = 0;
