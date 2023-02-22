a1d083b70445d694bae36dc6f9bf513e1f816532
Soluce7 <125985411+Soluce7@users.noreply.github.com>
2023-02-22 10:06:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-function rollDice() {
-  let dice1 = Math.floor(Math.random() * 6) + 1;
-  let dice2 = Math.floor(Math.random() * 6) + 1;
-  let dice3 = Math.floor(Math.random() * 6) + 1;
-  
-  let result1 = document.getElementById('result1');
-  let result2 = document.getElementById('result2');
-  let result3 = document.getElementById('result3');
-  
-  result1.innerHTML = dice1;
-  result2.innerHTML = dice2;
-  result3.innerHTML = dice3;
-}
+const rollBtn = document.getElementById("roll-btn");
+const diceElems = document.querySelectorAll(".dice");
 
-function displayDice() {
-  var die1 = document.getElementById("die1");
-  var die2 = document.getElementById("die2");
-  var die3 = document.getElementById("die3");
-  var d1 = Math.floor(Math.random() * 6) + 1;
-  var d2 = Math.floor(Math.random() * 6) + 1;
-  var d3 = Math.floor(Math.random() * 6) + 1;
-  var diceTotal = d1 + d2 + d3;
-  die1.innerHTML = d1;
-  die2.innerHTML = d2;
-  die3.innerHTML = d3;
-}
-
+rollBtn.addEventListener("click", () => {
+  diceElems.forEach((dice) => {
+    const randomValue = Math.floor(Math.random() * 6) + 1;
+    dice.textContent = randomValue;
+  });
+});
 
