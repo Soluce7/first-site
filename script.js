ead0639195d38e95064640f081f328320a6d1db9
Soluce7 <125985411+Soluce7@users.noreply.github.com>
2023-02-22 09:55:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function rollDice() {
   result3.innerHTML = dice3;
 }
 
+function displayDice() {
+  var die1 = document.getElementById("die1");
+  var die2 = document.getElementById("die2");
+  var die3 = document.getElementById("die3");
+  var d1 = Math.floor(Math.random() * 6) + 1;
+  var d2 = Math.floor(Math.random() * 6) + 1;
+  var d3 = Math.floor(Math.random() * 6) + 1;
+  var diceTotal = d1 + d2 + d3;
+  die1.innerHTML = d1;
+  die2.innerHTML = d2;
+  die3.innerHTML = d3;
+}
+
+
